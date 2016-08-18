b9c0c3b048b9c4ca2c7f5172646551fa99aef96d
Farrukh <webstyle@protonmail.com>
2016-08-18 22:53:45 +0500

    refactor

--- app.js
+++ app.js
@@ -39,6 +39,8 @@ app.post('/upload', function(req, res) {
     });
 });
 
-app.listen(3000, function() {
+var port = process.env.PORT || 3000;
+
+app.listen(port, function() {
     console.log('Example app listening on port 3000!');
 });
