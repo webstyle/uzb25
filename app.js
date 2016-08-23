d0089d286b23045153b4bb7ee17cf10a16e209e9
Farrukh <webstyle@protonmail.com>
2016-08-23 15:56:25 +0500

    refactor

--- app.js
+++ app.js
@@ -3,7 +3,22 @@ var fileUpload = require('express-fileupload');
 var fs = require('fs');
 var gm = require('gm');
 var async = require('async');
+
+var passport = require('passport');
+var FacebookStrategy = require('passport-facebook');
+
 var app = express();
+
+
+passport.use(new FacebookStrategy({
+        clientID: 1179014042141213,
+        clientSecret: "915a8f331012718516e73c2480f40aba",
+        callbackURL: "http://localhost:3000/auth/facebook/callback"
+    },
+    function(accessToken, refreshToken, profile, cb) {
+        console.log(arguments);
+    }
+));
 
 app.use(express.static('public'));
 app.use(express.static('uploads'));
@@ -30,6 +45,7 @@ app.post('/upload', function(req, res) {
             .command('composite')
             .in("-gravity", "center")
             .in("./uploads/uzb.png")
+            .blur(30, 30)
             .in("-gravity", "center")
             .in("./uploads/filename.jpg")
             .write("./uploads/result.jpg", function(e) {
@@ -39,8 +55,21 @@ app.post('/upload', function(req, res) {
     });
 });
 
+
+app.get('/auth/facebook',
+    passport.authenticate('facebook'));
+
+app.get('/auth/facebook/callback',
+    passport.authenticate('facebook', {
+        failureRedirect: '/login'
+    }),
+    function(req, res) {
+        // Successful authentication, redirect home.
+        res.redirect('/');
+    });
+
 var port = process.env.PORT || 3000;
 
 app.listen(port, function() {
-    console.log('Example app listening on port 3000!');
+    console.log('Example app listening on port', port);
 });
