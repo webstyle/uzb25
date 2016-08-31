fd3eee281967a5c5c414b0ea66a17f3c6a3058c2
Farrukh <webstyle@protonmail.com>
2016-09-01 04:22:58 +0500

    added ui

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var passport = require('passport');
 var FacebookStrategy = require('passport-facebook');
 require('./config/passport')(passport); // pass passport for configuration
 
+var graph = require('fbgraph');
 
 var Images = require('./models/images');
 var User = require('./models/user');
@@ -22,7 +23,7 @@ var User = require('./models/user');
 var port = process.env.PORT || 3000;
 var app = express();
 
-mongoose.connect('mongodb://localhost:27017/facebook'); // connect to our database
+mongoose.connect('mongodb://localhost:27017/facebook3'); // connect to our database
 
 app.use(express.static('result'));
 
@@ -36,7 +37,9 @@ app.use(bodyParser.urlencoded({
 }));
 
 // required for passport
-app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(session({
+    secret: 'ilovescotchscotchyscotchscotch'
+})); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
@@ -48,11 +51,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 // default options
 app.use(fileUpload());
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     res.render('index');
 });
 
-app.post('/upload', function(req, res) {
+app.post('/upload', isLoggedIn, function(req, res) {
     var sampleFile;
     var user = req.user;
 
@@ -69,51 +72,69 @@ app.post('/upload', function(req, res) {
         if (err) {
             return res.status(500).send(err);
         }
+        overlay({
+            path: newUploadedImagePath,
+            fileName: newFileName,
+            uuid: newUuid,
+            userId: user.facebook.id
+        }, function(err, image) {
+          console.log(err);
+            if (err) return res.send('Error');
 
-        overlay({path: newUploadedImagePath, fileName: newFileName, uuid: newUuid, userId: 123}, function (err, image) {
-            if(err) return res.send('Error');
-
-            console.log(image);
-            res.redirect('/' + image.fileName);
+            res.redirect('/view/' + image.uuid);
         });
     });
 });
 
-app.get('/users', function(req, res) {
-    User.find(function (err, users) {
-        if(err) return res.json(err);
-
-        res.json(users);
-    })
+app.get('/view/:uuid', function(req, res) {
+    if (!req.params.uuid) return res.redirect('/');
+    var uuid = req.params.uuid;
+    Images.findOne({
+        uuid: uuid
+    }, function(err, image) {
+        if (err) return res.json(err);
+        console.log(image);
+        res.render('result', image);
+    });
 });
 
-app.get('/images', function(req, res) {
-    Images.find(function (err, images) {
-        if(err) return res.json(err);
-
-        res.json(images);
-    })
-});
 
 // route for showing the profile page
 app.get('/profile', isLoggedIn, function(req, res) {
-    console.log('req.user', req.user);
-    res.render('profile.ejs', {
-        user : req.user // get the user out of session and pass to template
-    });
+    var user = req.user;
+    var params = {
+        fields: "picture"
+    };
+
+    graph
+        .setAccessToken(user.facebook.token)
+        .get(user.facebook.id + "/picture", params, function(err, resp) {
+            Images.find({userId:user.facebook.id},function(err, images) {
+                if (err) return res.json(err);
+                var haveImages = (images.length >= 1) ? true : false;
+                res.render('profile.ejs', {
+                    user: user,
+                    haveImages: haveImages,
+                    image: resp,
+                    images: images
+                });
+            })
+        });
 });
 
 // =====================================
 // FACEBOOK ROUTES =====================
 // =====================================
 // route for facebook authentication and login
-app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
+app.get('/auth/facebook', passport.authenticate('facebook', {
+    scope: 'email'
+}));
 
 // handle the callback after facebook has authenticated the user
 app.get('/auth/facebook/callback',
     passport.authenticate('facebook', {
-        successRedirect : '/profile',
-        failureRedirect : '/'
+        successRedirect: '/profile',
+        failureRedirect: '/'
     }));
 
 // route for logging out
@@ -137,8 +158,8 @@ function isLoggedIn(req, res, next) {
     res.redirect('/');
 }
 
-var posterUrl = ['./image-manipulation/1.png', './image-manipulation/uzb.png'];
-var result, newUuid, newFileName;
+var posterUrl = ['./image-manipulation/cover-uzb.png', './image-manipulation/1.png', './image-manipulation/uzb.png'];
+var result, newFileName;
 
 function overlay(image, callback) {
 
@@ -151,11 +172,11 @@ function overlay(image, callback) {
 
     sharp('./uploads/' + image.fileName)
         .background('#fff')
-        .overlayWith(posterUrl[1], {
+        .overlayWith(posterUrl[0], {
             gravity: sharp.gravity.south
         })
         .quality(90)
-        .toFile(resultFile, function (err, info) {
+        .toFile(resultFile, function(err, info) {
             if (err) return callback(err);
 
             var imageResults = [];
@@ -169,18 +190,16 @@ function overlay(image, callback) {
                 },
                 results: imageResults,
                 userId: image.userId,
-                created_at: new Date()
+                created_at: new Date(),
+                userId: image.userId
             };
             var newImages = new Images(toSave);
-            newImages.save(function (err, result) {
-               if(err) console.log(err);
+            newImages.save(function(err, result) {
+                if (err) console.log(err);
             });
 
             var toReturn = {
-                fileName: newFileName,
-                savedPathPath: result,
-                uuid: newUuid,
-                info: info
+                uuid: image.uuid,
             };
             callback(null, toReturn);
         })
